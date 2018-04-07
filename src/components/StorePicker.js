f8c6cebbadc067091d1b6eca97509eab78ed1940
Ivan <vostokboy87@gmail.com>
2018-04-07 21:53:15 +1000

    Test fix

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,11 +3,11 @@ import React from "react";
 class StorePicker extends React.Component{
   render(){
     return (
-      <from className="store-selector">
+      <form className="store-selector">
       <h2>Please Enter A Store</h2>  
       <input type='text' placeholder='Store Name' required/>
       <button type='submit'>Visit Here</button> 
-  </from>  
+  </form>  
     )
   }
 }
