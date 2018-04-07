f1c04ed8ac41bd75689a727d8343980c649da879
Ivan <vostokboy87@gmail.com>
2018-04-07 22:32:26 +1000

    4 new components: App, Header, Inventory and Order

---
+++ src/components/Order.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Order extends React.Component{
+    render(){
+        return(
+            <div className="order">Orderrrr!!!</div>
+        )
+    }
+}
+
+export default Order;
